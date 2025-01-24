b63084383ddf8cf96879de43ebd758bfca6264d7
hirusha-adi <hirushaadi@gmail.com>
2025-01-24 23:16:00 +0530

    a

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,10 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { user, isUserLoggedIn } from "../lib/pocketbase"
 
 const Home = () => {
+
+  const [userEmail, setUserEmail] = useState("")
+  const [userPassword, setUserPassword] = useState("")
 
   useEffect(() => {
     document.title = `Home`
@@ -9,8 +12,16 @@ const Home = () => {
 
   return (
     <>
-      <div className="grid grid-cols-3 border-2">
-        <div className=""></div>
+      <div className="grid grid-cols-3 border-2 border-black">
+        <div className="">{isUserLoggedIn ? "true" : "You are not logged in!"}</div>
+        <div className="">
+          {isUserLoggedIn ? "loggedin" : (
+            <>
+              <input type="text" placeholder="Enter your email" value={userEmail} onChange={(e) => setUserEmail(e.target.value)} />
+              <input type="text" placeholder="Enter your password" value={userPassword} onChange={(e) => setUserPassword(e.target.value)} />
+            </>
+          )}
+        </div>
       </div>
     </>
   );
